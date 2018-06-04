3496d2b3ba130b7f934d2df3bea6e52729fd9f22
Jed Peek <jedpeek@yahoo.com>
2018-06-04 22:14:39 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,14 @@
 names = ["Tim", "Tom", "Jim"]
 def hello_t(array)
+  if block_given?
   i = 0
   while i < array.length
     yield(array[i])
     i+=1
   end
   array
+else
+  "No block given!"
 end
 
 # call your method here!
